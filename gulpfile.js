323131105eeb8b7cd138b52b1eaef86a9ebec83c
= <=>
2018-04-06 15:16:30 +0100

    Initializing Git

---
+++ gulpfile.js
@@ -0,0 +1,25 @@
+const gulp = require('gulp'),
+      watch = require('gulp-watch'),
+      prefixer = require('gulp-autoprefixer'),
+      sass = require('gulp-ruby-sass'),
+      uglify = require('gulp-uglify');
+
+gulp.task('styles', function() {
+  sass('src/scss/*.sass', {style: 'expanded'})
+  .on('error', sass.logError)
+  .pipe(prefixer('last 2 version'))
+  .pipe(gulp.dest('build/css'))
+});
+
+gulp.task('scripts', function() {
+  gulp.src('src/js/*.js')
+  .pipe(uglify())
+  .pipe(gulp.dest('build/js'))
+})
+
+gulp.task('watch', function() {
+  gulp.watch('src/scss/*.sass', ['styles']);
+  gulp.watch('src/js/*.js', ['scripts']);
+})
+
+gulp.task('default', ['styles', 'scripts', 'watch'])
---
+++ src/js/app.js
@@ -0,0 +1,9 @@
+// First log out
+console.log('Hello world');
+
+// Second log out
+console.log('Hello world');
+
+document.querySelectorAll('.btn').addEventLister('click', function() {
+  console.log('hello world');
+})
