674c6fdd637d4356ce4798037883b60153241e3f
= <=>
2018-04-06 17:57:40 +0100

    Modified for index.html

--- gulpfile.js
+++ gulpfile.js
@@ -4,6 +4,11 @@ const gulp = require('gulp'),
       sass = require('gulp-ruby-sass'),
       uglify = require('gulp-uglify');
 
+gulp.task('HTML', function() {
+  gulp.src('src/index.html')
+  .pipe(gulp.dest('build/'))
+});
+
 gulp.task('styles', function() {
   sass('src/scss/*.sass', {style: 'expanded'})
   .on('error', sass.logError)
